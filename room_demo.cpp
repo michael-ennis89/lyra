94d338b7f5509c9efc0bb18277a95c7fd29acca0
EML16 <evanmlyon@gmail.com>
2017-10-12 02:06:40 +0200

    Data structure improvement
    New data structure for Room, Item and Interaction.

--- room_demo.cpp
+++ room_demo.cpp
@@ -1,5 +1,5 @@
 /*
-    MUST be compiled as 
+    MUST be compiled as
         g++ -std=c++11 room_demo.cpp Room.cpp
 
 */
@@ -7,20 +7,45 @@
 #include<string>
 #include<fstream>
 #include<iostream>
-#include "Room.hpp"
-
-using namespace std;
+#include "GameData.hpp"
 
 int main()
 {
-    Room room;
-    string line, delimit("#@");
-    
-    ifstream ifs("test_room.txt");
-    room.load(ifs, delimit);
 
-    ofstream ofs("out_room.txt");
-    room.save(ofs, delimit);
-    
+
+    Data::Room* my_room = new Data::Room("0.room");
+
+    std::cout << "ROOM 0 Data: \n";
+    my_room->printShort();
+    my_room->printLong();
+    my_room->printExitLong();
+    my_room->printExitShort();
+
+    std::cout << "\n";
+
+    std::vector<Data::Item*> itemsArray = Data::getItems(3);
+
+    std::cout << "Items Data: \n";
+    for(Data::Item* e:itemsArray){
+        if (e->good()){
+            e->printShort();
+            e->printLong();
+        }
+        delete e;
+    }
+    std::cout << "\n";
+    std::vector<Data::Interaction*> interactionsArray = Data::getInteractions(3);
+
+    std::cout << "Interactions Data: \n";
+    for(Data::Interaction* e:interactionsArray) {
+        if(e->good()){
+            e->printLong();
+            e->printShort();
+        }
+        delete e;
+    }
+
+    delete my_room;
+    char ch; std::cin>>ch;
     return 0;
 }
